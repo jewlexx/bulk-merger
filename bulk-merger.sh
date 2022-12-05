e11691e1a80fdb5c58eb52894f036a42113c47e5
Juliette Cordor <grassojames5@gmail.com>
2022-12-05 16:50:33 +1100

    fixes

--- bulk-merger.sh
+++ bulk-merger.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 
-cmd=$(basename "$0")
-[[ -z "$*" ]] && echo 'usage: ./[list|merge|merge_only|review] query_string' && exit 64
+cmd=$(basename "$1")
+[[ -z "$*" ]] && echo 'usage: ./merger [list|merge|merge_only|review] query_string' && exit 64
 [[ -r ".env" ]] && source .env
 
 export QUERY_STRING="$*"
