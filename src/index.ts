54f70316d605cf6e7b87c4d093ae3e5eeb503262
Juliette Cordor <grassojames5@gmail.com>
2022-12-06 11:19:21 +1100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import * as core from '@actions/core';
+import * as gh from '@actions/github';
 
 async function run(): Promise<void> {
   try {
     const token = core.getInput('repo-token', { required: true });
     const authors = core.getInput('authors');
     const title = core.getInput('title');
+
+    const kit = gh.getOctokit(token);
   } catch (err) {
     if (err instanceof Error) core.setFailed(err.message);
   }
