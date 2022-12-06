f0cded1994809631e8e469016b6ffb6a5ded95a8
Juliette Cordor <grassojames5@gmail.com>
2022-12-06 11:16:35 +1100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import * as core from '@actions/core';
 
-async function run(): Promise<void> {}
+async function run(): Promise<void> {
+  try {
+    const token = core.getInput('token', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    const owner = core.getInput('owner', { required: true });
+  } catch (err) {
+    core.setFailed(err.message);
+    return;
+  }
+}
 
 run();
